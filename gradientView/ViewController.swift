d29fccc03d3e2a60f97f8d36fcf8a44e34e8c8b3
Anatoliy Odinetskiy <odinetskiy92@gmail.com>
2024-02-03 21:04:53 +0700

    Initial Commit

---
+++ gradientView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gradientView
+//
+//  Created by Anatoliy Odinetskiy on 03.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
